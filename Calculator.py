9c7b58e5d8e18c2e3df71c9da127b90d9f10e2fe
Abhinav-41 <118150884+Abhinav-41@users.noreply.github.com>
2023-03-21 09:04:53 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+# Define the arithmetic functions
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+# Get user input
+print("Select operation.")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
