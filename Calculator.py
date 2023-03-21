d856a5f4921d29c90d755e37cd2a82a12f67ed95
AbhinavR-281103 <122429958+AbhinavR-281103@users.noreply.github.com>
2023-03-21 09:05:54 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -17,3 +17,25 @@ print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
+choice = input("Enter choice (1/2/3/4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Perform the selected arithmetic operation
+if choice == '1':
+    print(num1, "+", num2, "=", add(num1, num2))
+
+elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+
+elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1, num2))
+
+elif choice == '4':
+    print(num1, "/", num2, "=", divide(num1, num2))
+
+else:
+    print("Invalid input")
+    
+    
